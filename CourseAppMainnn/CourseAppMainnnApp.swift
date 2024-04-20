380c94d4f65e5971290e3dad80fae96ace052a47
elvisrexhaa <elvis2000@hotmail.co.uk>
2024-04-20 13:23:13 +0100

    Initial Commit

---
+++ CourseAppMainnn/CourseAppMainnnApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseAppMainnnApp.swift
+//  CourseAppMainnn
+//
+//  Created by Elvis Rexha on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CourseAppMainnnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
